f54d2025cc37b9efce4c19d508245f64962276f7
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-08 18:19:01 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,5 +44,5 @@ def get_average_age_for_season(data, season)
   data[season].each do |k, v|
     average_age << k["age"].to_i
   end
-  return average_age.reduce(:+) / average_age.length
+  return (average_age.reduce(:+) / average_age.length).ciel
 end
