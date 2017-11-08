191fc2704c5c76ffd83cfbda9f1b2f978519b3a4
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-08 16:08:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season.each do |k, v|
+    if k[v] == "Winner"
+      return 
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
